f2ef63b5d7d7a3313dae200a30c241e18b1448f6
tuanngocfun <tuanngoccs50@gmail.com>
2024-04-04 11:21:20 +0700

    book scraper

---
+++ draft1.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com"]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+        
+        for book in books:
+            yield {
+                'name': book.css('h3 a::text').get(),
+                'price': book.css('.product_price .price_color::text').get(),
+                'url': book.css('h3 a').attrib['href'],
+            }
+            
+        next_page = response.css('li.next a ::attr(href)').get()
+        
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url, callback=self.parse)
